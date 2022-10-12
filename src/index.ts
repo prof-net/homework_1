f31edbd617f029cf7fae399f982aec908bc82399
prof-net <pavel.1c@mail.ru>
2022-10-12 09:58:36 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express, {Request, Response} from 'express';
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello 333!')
+    res.send('Hello 333!');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
