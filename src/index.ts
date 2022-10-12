481456c5ffd678369116c3fffbff7d1c9359185f
prof-net <pavel.1c@mail.ru>
2022-10-12 11:19:50 +0300

    change message

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello 333!');
+    res.send('Hello world');
 })
 
 app.listen(port, () => {
